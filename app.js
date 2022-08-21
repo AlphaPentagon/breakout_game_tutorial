04f80ce5755a22ce9b6b36f13408830ef66714fe
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-21 21:14:34 +0100

    Added movable paddle.  Customised CSS and added title

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const canvas = document.querySelector("#myCanvas");
-
 const ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
+const ballRadius = 10;
+
+// ball speed/direction
 let dx = 2;
 let dy = -2;
 
-const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
+const paddleSpeed = 5;
+let paddleX = (canvas.width - paddleWidth) / 2; // starting position of the paddle
+
+let rightPressed = false;
+let leftPressed = false;
 
 const randomNumber = () => {
   return Math.floor(Math.random() * 256);
@@ -24,9 +32,18 @@ const drawBall = () => {
   ctx.closePath();
 };
 
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
 
@@ -41,5 +58,33 @@ const draw = () => {
     randomColour = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
     dy = -dy;
   }
+
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + paddleSpeed, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - paddleSpeed, 0);
+  }
 };
+
+const keyDownHandler = (e) => {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true;
+  }
+};
+
+const keyUpHandler = (e) => {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = false;
+  }
+};
+
+// listening for key presses and releases
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 setInterval(draw, 10);
